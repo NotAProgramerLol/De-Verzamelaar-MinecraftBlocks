e47e8ebbf9f20db9d872ff4f53fb116b102a3fdb
mitchellston <m.steenwijk@live.nl>
2022-09-19 17:47:19 +0200

    react query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import "./App.scss";
+
+const fetchSomething = async () => {
+  const res = await fetch("./THISDOESNOTEXIST.json");
+  return res.json();
+};
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const queryClient = useQueryClient();
+  const { data, status } = useQuery(["THISDOESNOTEXIST"], fetchSomething);
   return (
     <div className="App">
+      {status === "error" && <p>OMG THE FETCH FAILED</p>}
+      {status === "loading" && <p>TRYING TO GET DATA THAT DOES NOT EXIST</p>}
+
       <button>test</button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
