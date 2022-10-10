1d30c86d13c58678b59a5167319fa570426f6d57
mitchellston <m.steenwijk@live.nl>
2022-10-10 23:23:24 +0200

    add to collection backend

---
+++ api/private/addToCollection.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["HTTPS"] != "on") {
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
+header("Content-Type: application/json");
+require_once "../auth/checkUser.php";
+$items = $_POST["Items"];
+if ($items == "" || !isset($items)) {
+    echo json_encode(array("response" => "Failed", "data" => ["message" => "Er zijn geen items om toe te voegen"]));
+    exit;
+}
+for ($i = 0; $i < count($items); $i++) {
+    $checkAv = mysqli_query($dbConnection, "SELECT `ID`, `ProductAvailability` FROM `Products` WHERE `ID` = '" . $items[$i] . "'");
+    if (mysqli_num_rows($checkAv) == 0) {
+        echo json_encode(array("response" => "Failed", "data" => ["message" => "Een item dat je probeert te kopen is niet meer beschikbaar"]));
+        exit;
+    }
+}
+for ($i = 0; $i < count($items); $i++) {
+    $result = mysqli_query($dbConnection, "INSERT INTO `Collection` (`Order`,`Product`) VALUES ('" . $userDetails["ID"] . "', '" . $items[$i] . "') WHERE `ID` = '" . $items[$i] . "'");
+    if (!$result) {
+        echo json_encode(array("response" => "Failed", "data" => ["message" => "Iets is er fout gegaan!"]));
+        exit;
+    }
+    $checkAv = mysqli_query($dbConnection, "SELECT `ID`, `ProductAvailability` FROM `Products` WHERE `ID` = '" . $items[$i] . "'");
+    $checkAv = mysqli_fetch_assoc($checkAv);
+    $remove = mysqli_query($dbConnection, "UPDATE `Products` SET `ProductAvailability` = '" . (intval($checkAv["ProductAvailability"]) - 1) . "' WHERE `ID` = '" . $items[$i] . "'");
+}
+echo json_encode(array("response" => "Success", "data" => ["message" => "De items zijn toegevoegd"]));
