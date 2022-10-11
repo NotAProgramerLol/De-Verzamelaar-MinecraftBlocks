8d7681a96edcb9da6e24345bb9f15534752ecf42
mitchellston <m.steenwijk@live.nl>
2022-10-11 12:29:01 +0200

    admin page (delete and add product)

---
+++ components/admin/form.tsx
@@ -0,0 +1,67 @@
+import React, { FormEventHandler } from "react";
+import axios from "axios";
+type products = {
+  Name: string;
+  Image: string;
+  Price: number;
+  ID: number;
+  Availability: number;
+  Description: string;
+};
+const Form = (data: products) => {
+  // a local state to store the currently selected file.
+  const [ID, setID] = React.useState(data.ID);
+  const [Error, setError] = React.useState("");
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("ID", ID.toString());
+    try {
+      const response = await axios({
+        method: "post",
+        url: "https://87609.stu.sd-lab.nl/beroeps/verzamelaar/api/private/removeProduct.php",
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      if (response.data.response != "Failed") {
+        location.reload();
+        return;
+      }
+      setError(response.data.data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label>Naam: </label>
+
+        <input type="text" defaultValue={data.Name} />
+      </p>
+      <p>
+        <label>Description: </label>
+        <br />
+        <textarea defaultValue={data.Description}></textarea>
+      </p>
+      <p>
+        <label>Prijs: </label>
+
+        <input type="number" defaultValue={data.Price} />
+      </p>
+      <p>
+        <label>Hoeveelheid: </label>
+        <input type="number" defaultValue={data.Availability} />
+      </p>
+      <p>
+        <img src={data.Image} alt="" />
+      </p>
+      <p>
+        <input type="submit" value="delete" />
+      </p>
+    </form>
+  );
+};
+
+export default Form;
--- src/pages/AdminProducten.tsx
+++ src/pages/AdminProducten.tsx
@@ -1,11 +1,135 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import { json } from "stream/consumers";
 import Link from "../../components/general/Link";
 import local_css from "./css/AdminProducten.css?inline";
+import Form from "../../components/admin/form";
+type response = {
+  response: string;
+  data: products[];
+};
+type products = {
+  Name: string;
+  Image: string;
+  Price: number;
+  ID: number;
+  Availability: number;
+  Description: string;
+};
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [newName, setNewName] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [newPrice, setNewPrice] = useState("");
+  const [newAv, setNewAv] = useState("");
+  const [Error, setError] = useState("");
+  const { isLoading, error, data } = useQuery(
+    ["getProducts"],
+    async (): Promise<response | any> => {
+      const { data: resp } = await axios.get(
+        "https://87609.stu.sd-lab.nl/beroeps/verzamelaar/api/public/getProducts.php"
+      );
+      return resp;
+    }
+  );
+  console.log(data);
+  const handleFileSelect = (event: any) => {
+    setSelectedFile(event.target.files[0]);
+  };
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>An error has occurred!</p>;
   return (
-    <div className="App">
+    <div className="bg-white">
       <style>{local_css}</style>
-      <h1>Page does not exist</h1>
-      <Link href="index">Kaas</Link>
+      <h1>Nieuw product</h1>
+      <div>
+        <form
+          onSubmit={async (event: any) => {
+            event.preventDefault();
+            const formData = new FormData();
+            if (selectedFile != "") {
+              formData.append("img", selectedFile || "");
+            }
+            formData.append("name", newName);
+            formData.append("description", newDescription);
+            formData.append("price", newPrice);
+            formData.append("availability", newAv);
+            try {
+              const response = await axios({
+                method: "post",
+                url: "https://87609.stu.sd-lab.nl/beroeps/verzamelaar/api/private/addProduct.php",
+                data: formData,
+                headers: { "Content-Type": "multipart/form-data" },
+              });
+              if (response.data.response == "Failed") {
+                setError(response.data.data.message);
+              }
+            } catch (error) {
+              console.log(error);
+            }
+          }}
+        >
+          <p>
+            <label>Name</label>
+            <input
+              type="text"
+              onChange={(event) => {
+                setNewName(event.target.value);
+              }}
+            />
+          </p>
+          <p>
+            <label>Description</label>
+
+            <textarea
+              onChange={(event) => {
+                setNewDescription(event.target.value);
+              }}
+            ></textarea>
+          </p>
+          <p>
+            <label>Prijs</label>
+
+            <input
+              type="number"
+              onChange={(event) => {
+                setNewPrice(event.target.value);
+              }}
+            />
+          </p>
+          <p>
+            <label>Availability</label>
+
+            <input
+              type="number"
+              onChange={(event) => {
+                setNewAv(event.target.value);
+              }}
+            />
+          </p>
+          <p>
+            <label>Afbeelding</label>
+
+            <input type="file" onChange={handleFileSelect} />
+          </p>
+          <p>
+            <input type="submit" value="Nieuw product" />
+          </p>
+        </form>
+        <p>{Error}</p>
+      </div>
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Alle producten
+        </h2>
+
+        <div>
+          {data.data.map((product: products) => (
+            <Form {...product}></Form>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
